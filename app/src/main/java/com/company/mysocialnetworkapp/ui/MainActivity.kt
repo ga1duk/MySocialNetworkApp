8ac96c930dd19809b809375cd8aa8adaa8f3ccea
ga1duk <gaidukdg@yandex.ru>
2022-09-23 23:28:15 +0300

    unusual import are removed from MainActivity

--- app/src/main/java/com/company/mysocialnetworkapp/ui/MainActivity.kt
+++ app/src/main/java/com/company/mysocialnetworkapp/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.company.mysocialnetworkapp.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
